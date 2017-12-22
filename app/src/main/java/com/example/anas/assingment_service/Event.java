feacbdd2874e24fddabbddd97f2286b3c981b5c7
Anas8819 <anasahmed8819@hotmail.com>
2017-12-22 22:18:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/anas/assingment_service/Event.java
@@ -0,0 +1,21 @@
+package com.example.anas.assingment_service;
+
+/**
+ * Created by Anas on 11/11/2017.
+ */
+
+public class Event {
+    private int message;
+
+    public int getMessage() {
+        return message;
+    }
+
+    public Event(int message) {
+        this.message = message;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
+}
